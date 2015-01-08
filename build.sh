69f52eecb8d8b48103c0f0f0e13c4b9d868ed58c
Simon Frankau <sgf@arbitrary.name>
2015-01-08 23:18:05 +0000

    Tidied up the build process a bit

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p out
 
+# Compile source
 cd src
 
-zasm bouncer.ass -o ../out/bouncer.bin
-zasm test.ass    -o ../out/test.rom
-zasm monitor.ass -o ../out/monitor.bin
+zasm bouncer.ass -o ../out/bouncer.s
+zasm test.ass    -o ../out/test.s
+zasm monitor.ass -o ../out/monitor.s
+
+# Build ROM images
 
 cd ../out
 
-lua ../tools/build_test_data.lua -s 0x7000 -m 17 -o testdata.rom
-lua ../tools/build_rom.lua -o z80.bin test.rom:0x0000 testdata.rom:0x1000
+lua ../tools/build_test_data.lua -s 0x7000 -m 17 -o testdata.s
+
+lua ../tools/build_rom.lua -o bouncer.bin bouncer.s:0x0000
+lua ../tools/build_rom.lua -o test.bin    test.s:0x0000 testdata.s:0x1000
+lua ../tools/build_rom.lua -o monitor.bin monitor.s:0x0000
