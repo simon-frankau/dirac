dcf597ce9680fe9d60ff977f869db334dd8fdfd1
Simon Frankau <sgf@arbitrary.name>
2016-05-02 20:34:02 +0100

    build_hex.lua now includes the monitor header line.

--- tools/build_hex.lua
+++ tools/build_hex.lua
@@ -1,8 +1,12 @@
 -- Create a hex file which can be transmitted to the monitor over serial.
 --
+-- The monitor maps one bank of RAM to 0xFxxx, so we assume by default
+-- that's where you want your data placing.
+
 -- Command line arguments are:
 -- -o file_name           Output filename
 -- file_name              Input filename
+-- -t dest                (Optional) target address in hex
 
 ------------------------------------------------------------------------
 -- Config
@@ -10,6 +14,7 @@
 
 local in_file  = nil
 local out_file = nil
+local target = 0xF000
 
 do
     local i = 1
@@ -17,6 +22,9 @@ do
         if arg[i] == "-o" then
             i = i + 1
             out_file = arg[i]
+        elseif arg[i] == "-t" then
+            i = i + 1
+            target = tonumber(arg[i], 16)
         else
             in_file = arg[i]
         end
@@ -44,9 +52,15 @@ f_in:close()
 
 local f_out = assert(io.open(out_file, "wb"))
 
+f_out:write(string.format("W %04x %04x\n", target, content:len()))
+
 for i = 1, #content do
    local c = content:byte(i)
    f_out:write(string.format("%02x%s", c, i % 16 ~= 0 and " " or "\n"))
+end
+
+if content:len() % 16 ~= 0 then
+    f_out:write("\n")
 end
 
 f_out:close()
