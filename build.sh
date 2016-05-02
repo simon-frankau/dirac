4f16f66148725c17ecea79c388485d0a38f744dc
Simon Frankau <sgf@arbitrary.name>
2016-05-02 17:15:27 +0100

    Ooops, should actually build example.txt

--- build.sh
+++ build.sh
@@ -21,3 +21,4 @@ lua ../tools/build_test_data.lua -s 0x7000 -m 17 -o testdata.s
 lua ../tools/build_rom.lua -o bouncer.bin bouncer.s:0x0000
 lua ../tools/build_rom.lua -o test.bin    test.s:0x0000 testdata.s:0x1000
 lua ../tools/build_rom.lua -o monitor.bin monitor.s:0x0000
+lua ../tools/build_hex.lua -o example.txt example.s
